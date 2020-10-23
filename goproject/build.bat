9dbdbc0dcf0185060da352de7fa775e5c01718c8
zhangbaowei <zbw911@gmail.com>
2020-10-23 19:20:17 +0800

    init

--- goproject/build.bat
+++ goproject/build.bat
@@ -1,3 +1,3 @@
 
- wsl go build -buildmode=c-shared -o /mnt/h/autohomegit/GP_NEW_PROJECT/main-content/grpc/src/main/resources/linux-x86-64/liblibgoredis.so main.go
- go build  -buildmode=c-shared -o H:\autohomegit\GP_NEW_PROJECT\main-content\grpc\src\main\resources\win32-x86-64\libgoredis.dll main.go
+     wsl go build -buildmode=c-shared -o ../javaproject/src/main/resources/linux-x86-64/liblibgoredis.so main.go
+      go build -buildmode=c-shared -o ..\javaproject\src\main\resources\win32-x86-64\libgoredis.dll main.go
--- goproject/build2.bat
+++
@@ -1,3 +0,0 @@
-
- wsl go build -buildmode=c-shared -o /mnt/h/autohomegit/zhangbaowei/javacallgo/src/main/resources/linux-x86-64/liblibgoredis.so main.go
- go build  -buildmode=c-shared -o H:\autohomegit\zhangbaowei\javacallgo\src\main\resources\win32-x86-64\libgoredis.dll main.go
